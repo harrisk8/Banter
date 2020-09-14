5fa26fd18e7f20adea249942fcae31fa519ce40d
Harris Kapoor <harriskkkapoor@gmail.com>
2020-09-14 03:00:23 -0400

    9/14/2020

--- Mesh/Table View Cells/TrendingTableViewCell.swift
+++ Mesh/Table View Cells/TrendingTableViewCell.swift
@@ -9,16 +9,98 @@
 import UIKit
 
 class TrendingTableViewCell: UITableViewCell {
-
+    
+    
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var postScore: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+    
+    
+    var likedPost = false
+    var dislikedPost = false
+    
+    var likedFromDislike = true
+    var dislikedFromLike = false
+    
+    var randomInt = 0
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        
+        randomInt = Int.random(in: 1...100)
+        postScore?.text = String(randomInt)
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func likeButtonPressed(_ sender: Any) {
+        
+        if dislikedPost == true {
+            print("DISLIKE -> LIKE")
+            randomInt += 2
+            likeButton.setImage(UIImage(named: "Like Button Selected"), for: .normal)
+            dislikeButton.setImage(UIImage(named: "Dislike Button Regular"), for: .normal)
+            dislikedPost = false
+            likedPost = true
+
+        }
+        
+        if likedPost == false {
+            print("LIKE")
+            randomInt += 1
+            likeButton.setImage(UIImage(named: "Like Button Selected"), for: .normal)
+            postScore?.text = String(randomInt)
+            likedPost = true
+        } else if likedPost == true {
+            print("Removing Like")
+            randomInt -= 1
+            likeButton.setImage(UIImage(named: "Like Button Regular"), for: .normal)
+            postScore?.text = String(randomInt)
+            likedPost = false
+        }
+    }
+    
+    
+    @IBAction func dislikeButtonPressed(_ sender: Any) {
+        if likedPost == true {
+            randomInt -= 2
+            dislikeButton.setImage(UIImage(named: "Dislike Button Selected"), for: .normal)
+            likeButton.setImage(UIImage(named: "Like Button Regular"), for: .normal)
+            likedPost = false
+            dislikedPost = true
+            
+        }
+        
+        
+        if dislikedPost == false {
+            print("DISLIKE")
+            randomInt -= 1
+            dislikeButton.setImage(UIImage(named: "Dislike Button Selected"), for: .normal)
+            postScore?.text = String(randomInt)
+            dislikedPost = true
+        } else if dislikedPost == true {
+            print("Removing Dislike")
+            randomInt += 1
+            dislikeButton.setImage(UIImage(named: "Dislike Button Regular"), for: .normal)
+            postScore?.text = String(randomInt)
+            dislikedPost = false
+        }
+        
+        
     }
     
 }
--- Mesh/View Controllers/CommentsViewController.swift
+++ Mesh/View Controllers/CommentsViewController.swift
@@ -7,30 +7,54 @@
 //
 
 import UIKit
+import QuartzCore
 
-class CommentsViewController: UIViewController, UITextViewDelegate {
+class CommentsViewController: UIViewController, UITextViewDelegate, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var commentEditorView: UIView!
     
-    @IBOutlet weak var postMessage: UILabel!
+    @IBOutlet weak var postMessage: UITextView!
+    @IBOutlet weak var commentsTextView: UITextView!
     
-    @IBOutlet weak var commentTextView: UITextView!
+    
+    
+    @IBOutlet weak var commentsEditorView: UIView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    @IBOutlet weak var commentsTableView: UITableView!
+    
     
     var postArrayPosition: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        commentTextView.delegate = self
-        
         postMessage.text = NearbyArray.nearbyArray[postArrayPosition ?? 0].message
+
+        
+        commentsTableView.dataSource = self
+        commentsTableView.delegate = self
+        
+        commentsTableView.register(UINib(nibName: "NearbyTableViewCell", bundle: nil), forCellReuseIdentifier: "NearbyTableCellIdentifier")
+        
+        commentsTableView.estimatedRowHeight = 150;
+        commentsTableView.rowHeight = UITableView.automaticDimension;
+        
+        commentsTableView.layoutMargins = .zero
+        commentsTableView.separatorInset = .zero
+        
+        commentsTextView.delegate = self
+        commentsTextView.backgroundColor = UIColor.white
+        commentsTextView.layer.cornerRadius = 5.0
+        commentsTextView.clipsToBounds = true
 
     }
     
     func slideCommentEditorUp() {
         
         UIView.animate(withDuration: 0.3) {
-            self.commentEditorView.frame.origin.y -= CGFloat(UserInfo.keyboardHeight ?? 0)
+            self.commentsEditorView.frame.origin.y -= CGFloat(UserInfo.keyboardHeight ?? 0) + CGFloat(self.commentsTextView.frame.height)
         }
     
     }
@@ -45,4 +69,48 @@ class CommentsViewController: UIViewController, UITextViewDelegate {
         dismiss(animated: true, completion: nil)
     }
     
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(NearbyArray.nearbyArray.count)
+        return NearbyArray.nearbyArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let nearbyCellData = NearbyArray.nearbyArray[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NearbyTableCellIdentifier", for: indexPath) as! NearbyTableViewCell
+        cell.authorLabel?.text = String(nearbyCellData.author! )
+        cell.messageLabel?.text = String(nearbyCellData.message! )
+        cell.timestampLabel?.text = formatPostTime(postTimestamp: nearbyCellData.timestamp!)
+        
+        return cell
+        
+    }
+    
+    //Converts timestamp from 'seconds since 1970' to readable format
+    func formatPostTime(postTimestamp: Double) -> String {
+        
+        let timeDifference = (UserInfo.refreshTime ?? 0.0) - postTimestamp
+        
+        let timeInMinutes = Int((timeDifference / 60.0))
+        let timeInHours = Int(timeInMinutes / 60)
+        let timeInDays = Int(timeInHours / 24)
+        
+        if timeInMinutes < 60 {
+            return (String(timeInMinutes) + "m")
+        } else if timeInMinutes >= 60 && timeInHours < 23 {
+            return (String(timeInHours) + "h")
+        } else {
+            return (String(timeInDays) + "d")
+        }
+        
+    
+    }
 }
